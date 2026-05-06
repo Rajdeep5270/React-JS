60cf19bb13f90118edf745ac325737daa2229ddc
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-05-06 17:55:24 +0530

    My Next CRUD App

--- my-next-crud-app/app/layout.tsx
+++ my-next-crud-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ToastContainer } from "react-toastify";
 import Header from "./components/Header";
 import "./globals.css"
+import "react-toastify/dist/ReactToastify.css";
 
 export default function RootLayout({
   children,
@@ -12,11 +13,28 @@ export default function RootLayout({
       lang="en"
       className={`h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <head>
+        <title>Bikes. - Manage Your Motorcycle Collection</title>
+        <meta name="description" content="Complete motorcycle management platform" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className="min-h-full flex flex-col bg-white">
         <Header />
-        {children}
-
-        <ToastContainer />
+        <main className="flex-1">
+          {children}
+        </main>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={true}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </body>
     </html>
   );
